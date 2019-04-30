90bedc9cac9f255ad7876a96596f3b40542f875f
unknown <andrescardenas34@outlook.com>
2019-04-29 23:54:53 -0400

    sign up component

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react'
 class SignUpForm extends Component {
     render(){
         return (
-
+<div>
+    <h1>Hello there! sign up here!</h1>
             <form onSubmit={this.props.hasSignedUp}>
                 <input type="text" placeholder='Email' />
                 <input type='password' placeholder='Password'/>
                 <button type='submit'>sign up!</button>
             </form>
-
+</div>
         )
     }
 }
